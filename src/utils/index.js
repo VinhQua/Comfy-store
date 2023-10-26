b58cb9c1ce00162f1c853ac5fb0c3f9b22de0b0f
VinhQua <dragonlinuxnoinochi@gmai.com>
2023-10-26 15:46:06 +0900

    fix price

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,10 +1,10 @@
 import { customFetch } from "./axios";
 export { customFetch };
 
-export const formatPrice = (price: any) => {
+export const formatPrice = (price) => {
   const dollarsAmount = new Intl.NumberFormat("en-Us", {
     style: "currency",
     currency: "USD",
-  }).format((price / 100).toFixed(2)) as any;
+  }).format((price / 100).toFixed(2));
   return dollarsAmount;
 };
